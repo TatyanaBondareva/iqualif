c00994a3f6e838e8df568d77579baefaa0c1884e
Tatiana Bondareva <tbondareva@oggettoweb.com>
2019-08-20 12:43:54 +0300

    Make mobile and tablet@

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('.js-header-menu-link-list').on("click", openList);
     $('.js-header-menu-sublist-link').on("click", changeCurrency);
     $('.js-header-menu-sublist-lang').on("click", changeLang);
+    $('.js-footer-links-title').on('click', toggleLinks);
 
 	$('#closeMenu').on("click", closeMobileMenu);
 	$('#openMenu').on("click", openMobileMenu);
@@ -27,14 +28,27 @@ function closeSearch() {
 }
 
 function openMobileMenu() {
-	$('#headerMobileMenu').removeClass('mobile-list_hiden');
-	$("#headerMobileMenu").css('animation', 'blick 0.8s  ease-in-out');
+	$('#headerMobileMenu').show(800);
 }
 function closeMobileMenu() {
-	$('#headerMobileMenu').addClass('mobile-list_hiden');
+	$('#headerMobileMenu').hide(800);
 }
 function getAllList() {
 	console.log($(this.closest('.playlist')).children('.video-wrapper').children('.video-block'));
 	$(this.closest('.playlist')).children('.video-wrapper').children('.video-block').css({'display': 'flex', 'animation': 'blick 1s  ease-in-out'});
 	$(this).prop( "disabled", true ).css('opacity','0.5');
 }
+function toggleLinks() {
+    if(screen.width > 768) {
+        return;
+    }
+    else {
+        $(this).next('.js-footer-links-list').slideToggle();
+        if($(this).children('.footer-links__title-span').text() == '+') {
+            $(this).children('.footer-links__title-span').text('-');
+		}
+		else {
+            $(this).children('.footer-links__title-span').text('+');
+		}
+	}
+	}
